98743e224e36937f9317de87bd80094209064533
GARBES <129992854+garbesc@users.noreply.github.com>
2023-05-15 02:59:44 +0200

    Add files via upload
    Test Frontend sur Heroku

---
+++ tag-me.py
@@ -0,0 +1,136 @@
+# Librairies
+import streamlit as st
+import numpy as np
+import re
+import pickle
+import requests
+import json
+
+def request_prediction(model_uri, data):
+    headers = {"Content-Type": "application/json"}
+
+    data_json = {'Body': data}
+    response = requests.request(
+        method='POST', headers=headers, url=model_uri, json=data_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
+
+# Chargement du Vectorizer 
+def load_vect(add_vect):   
+    file_vect = open(add_vect, 'rb')
+    vect = pickle.load(file_vect)
+    return vect
+
+# Chargement du multiLabelBinarizer pré-entrainé
+def load_mlb(add_mlb): 
+    file_mlb = open(add_mlb, 'rb')
+    mlb = pickle.load(file_mlb)
+    return mlb
+
+# Tokenizer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def tokenizer_fct(sentence) :
+    # print(sentence)
+    sentence_clean = sentence.replace('-', ' ').replace('+', ' ').replace('/', ' ').\
+                             replace('#', ' ').replace('<p>', ' ').replace('>', ' ').replace('<', ' ')
+    # Remove ponctuation (except # and ++ for c# and c++)
+    sentence_clean1 = re.sub('[^\\w\\s#\\s++]', ' ', sentence_clean)
+
+    # Remove numbers
+    sentence_clean2 = re.sub(r'\w*\d+\w*', ' ', sentence_clean1)
+
+    # Remove extra spaces
+    sentence_clean = re.sub('\s+', ' ', sentence_clean2)
+    word_tokens = word_tokenize(sentence_clean)
+    return word_tokens
+
+# Stop words
+from nltk.corpus import stopwords
+stop_w = list(set(stopwords.words('english'))) + ['[', ']', ',', '.', ':', '?', '(', ')']
+stop_w.extend(['code', 'quot', 'use', 'http', 'com', 'error', 'work', 'want', 'one', 'would', 'need', 
+                   'help', 'also', 'exampl', 'could', 'thing', 'well', 'dear', 'p'])
+
+def stop_word_filter_fct(list_words) :
+    filtered_w = [w for w in list_words if not w in stop_w]
+#    filtered_w2 = [w for w in filtered_w if len(w) > 2]
+    return filtered_w
+
+# lower case et alpha
+def lower_start_fct(list_words) :
+    lw = [w.lower() for w in list_words if (not w.startswith("@")) 
+                                      and (not w.startswith("#"))
+                                       and (not w.startswith("http"))]
+    return lw
+
+# Lemmatizer (base d'un mot)
+from nltk.stem import WordNetLemmatizer
+
+def lemma_fct(list_words) :
+    lemmatizer = WordNetLemmatizer()
+    lem_w = [lemmatizer.lemmatize(w) for w in list_words]
+    return lem_w
+
+# Fonction de préparation du texte pour le bag of words (Countvectorizer et Tf_idf, Word2Vec)
+def transform_bow_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+    sw = stop_word_filter_fct(word_tokens)
+    lw = lower_start_fct(sw)
+    # lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(lw)
+    return transf_desc_text
+
+def process_text(text):
+    # mise en forme du texte
+    text_prep = transform_bow_fct(text)
+    text_split = ["".join(word) for word in text_prep.split(" ")]
+    final_text = [np.array(text_split, dtype='<U41')]
+    return text_split, final_text
+    
+def fetch_tag(text):
+    y_pred_inversed = mlb.inverse_transform(text)
+    return y_pred_inversed
+
+
+def main():
+    MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+
+    # Chargement du vectorizer, multiLablbinarizer
+    #vect = load_vect("./models/vect.pkl")
+#    mlb = load_mlb("./models/mlb.pkl")
+    
+    st.title("Catégoriser automatiquement une question")
+
+    input_text = st.text_input('Question')
+
+    formatted_text, final_text = process_text(input_text)
+    st.write('Texte formaté : ', formatted_text)
+
+    if st.button('Rechercher les tags'):
+        arr = np.array([["pyhton"]])
+
+        arr_as_list = arr.tolist()
+        json_data = json.dumps(arr_as_list)
+        st.success(json_data)
+        pred = request_prediction(MLFLOW_URI, json_data)[0] * 100000
+        st.success(pred)
+#        pred_txt = fetch_tag(pred)
+#        st.success(pred_txt, icon="✅")
+
+#       version locale
+        file_pipe = open("./models/pipeline.pkl", 'rb')
+        file_lbb = open("./models/mlb.pkl", 'rb')
+        pipe = pickle.load(file_pipe)
+        pred = pipe.predict(final_text)
+        
+        lbb = pickle.load(file_lbb)
+        pred_txt = lbb.inverse_transform(pred)
+        st.success(pred_txt, icon="✅")
+
+if __name__ == '__main__':
+    main()
